68848aa304ab0e1324f810919d7866327aee885e
ITQ <itq.dev@ya.ru>
2026-02-01 22:19:03 +0300

    feat(adapters): added UserRepository implementation

---
+++ project/src/main/kotlin/com/project/MovieNight/adapters/persistence/inmemory/UserRepositoryAdapter.kt
@@ -0,0 +1,25 @@
+package com.project.movienight.adapters.persistence.inmemory
+
+import com.project.movienight.application.ports.output.UserRepositoryPort
+import com.project.movienight.domain.model.User
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+class UserRepositoryAdapter : UserRepositoryPort {
+    private val users: MutableMap<UUID, User> = mutableMapOf()
+
+    override fun save(user: User): User {
+        users[user.id] = user
+
+        return user
+    }
+
+    override fun findById(id: UUID): User? = users[id]
+
+    override fun findAll(): List<User> = users.values.toList()
+
+    override fun deleteById(id: UUID) {
+        users.remove(id)
+    }
+}
