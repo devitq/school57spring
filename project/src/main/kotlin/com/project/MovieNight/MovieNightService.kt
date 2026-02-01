d3df436fb6b5969bd7fc61fcd2871db96238fe0a
ITQ <itq.dev@ya.ru>
2026-02-01 22:20:53 +0300

    feat(demo): added sample command line service

---
+++ project/src/main/kotlin/com/project/MovieNight/MovieNightService.kt
@@ -0,0 +1,29 @@
+package com.project.movienight
+
+import com.project.movienight.application.ports.input.CreateUserCommand
+import com.project.movienight.application.services.UserService
+import org.springframework.boot.CommandLineRunner
+import org.springframework.stereotype.Component
+
+@Component
+class MovieNightService(
+    private val userService : UserService,
+
+) : CommandLineRunner {
+
+    fun performActions() {
+        println("Создание пользователей...")
+        val userAboba = userService.create(CreateUserCommand(name="абоба", email = "aboba228@mail.com"))
+        val userAlice = userService.create(CreateUserCommand(name="алиса", email = "alice@mail.com"))
+        val userBob = userService.create(CreateUserCommand(name="боб", email = "bob@mail.com"))
+
+        println(userAboba)
+        println(userAlice)
+        println(userBob)
+    }
+
+    override fun run(vararg args: String) {
+        println("Запустился сервис")
+        performActions()
+    }
+}
